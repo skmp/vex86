5ad75b3f1c3c8fe85d438cd3de317300c7cec7be
Stefanos Kornilios Mitsis Poiitidis <skmp@nilware.io>
2023-08-10 05:22:05 +0300

    ainte, kamei patch k kana lea me rip tora, ta leme ali mera

--- main.cpp
+++ main.cpp
@@ -33,8 +33,14 @@ thread_local uint64_t state[16];
 
 static thread_local uint64_t tls_temp;
 
-uint64_t* tls_ofs() {
-    return &tls_temp;
+uint64_t get_tls_tmp_ofs() {
+    uint64_t out;
+    asm
+    (
+      "leaq _ZL8tls_temp@TPOFF, %0 \n"
+      :"=r"(out)
+    );
+    return out;
 }
 
 [[noreturn]]		
@@ -80,20 +86,93 @@ MetafrasmenosKodikas* metafrasi_kodika(uint8_t* diefthinsi_eisodou) {
         if (instruction.info.attributes & ZYDIS_ATTRIB_IS_RELATIVE) {
             
 
-            for (int i = 0; i < ZYDIS_MAX_OPERAND_COUNT; i++) { 
+            for (int i = 0; i < ZYDIS_MAX_OPERAND_COUNT; i++) {
                 if (instruction.operands[i].encoding == ZYDIS_OPERAND_ENCODING_MODRM_RM) {
 
                     if (instruction.operands[i].mem.base == ZYDIS_REGISTER_RIP) {
+                        auto real_addy = instruction.operands[i].mem.disp.value + runtime_address + instruction.info.length;
                         printf("operand: %d, encoding: %d\n", i, instruction.operands[i].encoding);
                         printf("details: base: %d disp: %lX val: %lX type: %d \n",
                         (int)instruction.operands[i].mem.base,
                         instruction.operands[i].mem.disp.value,
-                        instruction.operands[i].mem.disp.value + runtime_address + instruction.info.length,
+                        real_addy,
                         instruction.operands[i].mem.type);
 
                         assert(i==1);
 
-                        //
+                        //XCHG    %rax,%fs:~
+                        *buffer++ = 0x64;
+                        *buffer++ = 0x48;
+                        *buffer++ = 0x87;
+                        *buffer++ = 0x04;
+                        *buffer++ = 0x25;
+
+                        auto tls_offs = get_tls_tmp_ofs();
+                        *buffer++ = reinterpret_cast<uint64_t>(&tls_offs) >> 0;
+                        *buffer++ = reinterpret_cast<uint64_t>(&tls_offs) >> 8;
+                        *buffer++ = reinterpret_cast<uint64_t>(&tls_offs) >> 16;
+                        *buffer++ = reinterpret_cast<uint64_t>(&tls_offs) >> 24;
+
+                        // mov rax, helper
+                        *buffer++ = 0x48;
+                        *buffer++ = 0xb8;
+
+                        *buffer++ = reinterpret_cast<uint64_t>(&real_addy) >> 0;
+                        *buffer++ = reinterpret_cast<uint64_t>(&real_addy) >> 8;
+                        *buffer++ = reinterpret_cast<uint64_t>(&real_addy) >> 16;
+                        *buffer++ = reinterpret_cast<uint64_t>(&real_addy) >> 24;
+                        *buffer++ = reinterpret_cast<uint64_t>(&real_addy) >> 32;
+                        *buffer++ = reinterpret_cast<uint64_t>(&real_addy) >> 36;
+                        *buffer++ = reinterpret_cast<uint64_t>(&real_addy) >> 48;
+                        *buffer++ = reinterpret_cast<uint64_t>(&real_addy) >> 56;
+
+                        // now replace rip with rax
+                        ZydisEncoderRequest req;
+                        memset(&req, 0, sizeof(req));
+
+                        req.mnemonic = instruction.info.mnemonic;
+                        req.machine_mode = instruction.info.machine_mode;
+                        req.operand_count = instruction.info.operand_count;
+
+                        assert(instruction.operands[0].type == ZYDIS_OPERAND_TYPE_REGISTER);
+
+                        req.operands[0].type = ZYDIS_OPERAND_TYPE_REGISTER;
+                        req.operands[0].reg.value = instruction.operands[0].reg.value;
+                        
+                        req.operands[1].type = ZYDIS_OPERAND_TYPE_MEMORY;
+                        req.operands[1].mem.base = ZYDIS_REGISTER_RAX;
+                        req.operands[1].mem.displacement = 0;
+                        req.operands[1].mem.index = ZYDIS_REGISTER_NONE;
+                        req.operands[1].mem.scale = 0;
+                        req.operands[1].mem.size = instruction.info.operand_width / 8;
+
+                        ZyanUSize encoded_length = 15;
+
+                        if (ZYAN_FAILED(ZydisEncoderEncodeInstruction(&req, buffer, &encoded_length)))
+                        {
+                            puts("Failed to encode instruction");
+                            exit(1);
+                        }
+
+                        buffer += encoded_length;
+
+                        // now all done, swap back tmp
+                        //XCHG    %rax,%fs:~
+                        *buffer++ = 0x64;
+                        *buffer++ = 0x48;
+                        *buffer++ = 0x87;
+                        *buffer++ = 0x04;
+                        *buffer++ = 0x25;
+
+                        *buffer++ = reinterpret_cast<uint64_t>(&tls_offs) >> 0;
+                        *buffer++ = reinterpret_cast<uint64_t>(&tls_offs) >> 8;
+                        *buffer++ = reinterpret_cast<uint64_t>(&tls_offs) >> 16;
+                        *buffer++ = reinterpret_cast<uint64_t>(&tls_offs) >> 24;
+
+                        // Processed this one!
+                        offset += instruction.info.length;
+                        runtime_address += instruction.info.length;
+                        continue;
                     }
                 }
             }
